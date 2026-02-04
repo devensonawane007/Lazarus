aba71487031f2dbb89bdbc84621c249140594ff5
devensonawane007 <devenvsonawane007@gmail.com>
2026-02-04 16:15:56 +0530

    basic login

--- Lazarus-main/frontend/src/App.jsx
+++ Lazarus-main/frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import CreatorDashboard from "./pages/CreatorDashboard";
+import BrandDashboard from "./pages/BrandDashboard";
+import InvestorDashboard from "./pages/InvestorDashboard";
 import CreateProject from "./pages/CreateProject";
 import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CreateProject />} />
+      <Route path="/" element={<Login />} />
+
+      <Route path="/creator" element={<CreatorDashboard />} />
+      <Route path="/brand" element={<BrandDashboard />} />
+      <Route path="/investor" element={<InvestorDashboard />} />
+
+      {/* Existing routes */}
+      <Route path="/create-project" element={<CreateProject />} />
       <Route path="/project" element={<ProjectPage />} />
     </Routes>
   );
